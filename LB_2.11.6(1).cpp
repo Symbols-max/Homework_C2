a31a61666f8d9caf401cf10a6d0f10065d15738c
Symbols-max <56607745+Symbols-max@users.noreply.github.com>
2020-12-08 08:28:48 +0200

    Add files via upload

---
+++ LB_2.11.6(1).cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct Time{
+    int hours,mins,period,time;
+};
+
+int main() {
+  Time in;
+  cout<<"Enter hours: ";
+  cin>>in.hours;
+  cout<<"Enter mins: ";
+  cin>>in.mins;
+  cout<<"Enter period of time: ";
+  cin>>in.period;
+  
+  Time math;
+  math.time=in.hours*60+in.mins+in.period;
+  math.hours=math.time/60;
+  math.mins=math.time%60;
+  while(math.hours>=24){
+      math.hours=math.hours%24;
+  }
+  cout<<math.hours<<":"<<math.mins;
+  
+}
---
+++ LB_2.4.23(1).cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+
+int main(){
+    
+    int number;
+    label1:
+    cout<<"Введите число:\n";
+    cin>>number;
+    int sum=0;
+    if(number>=0)
+    {
+        
+        while(number)
+        {
+            sum=sum+(number&1);
+            number>>=1;
+        }
+        cout<<sum;
+    }
+    else
+    {
+        cout<<"Неправильное число, введите ещё раз";
+        goto label1;
+    }
+    
+}
